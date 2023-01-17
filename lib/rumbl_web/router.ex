89fdce438456667de66334db674f8ad966e04f4d
brave721 <narcissus721@gmail.com>
2023-01-17 08:27:24 +0000

    ch9. watching videos

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,11 +16,13 @@ defmodule RumblWeb.Router do
   end
 
   scope "/", RumblWeb do
-    pipe_through(:browser)
+    # Use the default browser stack
+    pipe_through :browser
 
-    get("/", PageController, :index)
-    resources("/users", UserController, only: [:index, :show, :new, :create])
-    resources("/sessions", SessionController, only: [:new, :create, :delete])
+    get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", RumblWeb do
